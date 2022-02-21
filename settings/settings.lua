8f4adae717980e9b495ca4a19befed25330efd6a
MirroxGame <70598667+MirroxGame@users.noreply.github.com>
2022-02-21 18:17:33 +0300

    Update settings.lua

--- settings/settings.lua
+++ settings/settings.lua
@@ -10,18 +10,22 @@ assert(readfile,"Your exploit does not support readfile function.")
 -- //functions
 
 getgenv().Load = function(FileName)
-return HP:JSONDecode(readfile(FileName))
+  return HP:JSONDecode(readfile(FileName))
 end
 
 getgenv().Set = function(FileName,Settings)
-if FileName:match("/") then
-makefolder(FileName:sub(1,FileName:find("/") - 1))
-end
-writefile(FileName, HP:JSONEncode(Settings))
-Load(FileName,Settings)
+  if FileName:match("/") then
+    makefolder(FileName:sub(1,FileName:find("/") - 1))
+  end
+    writefile(FileName, HP:JSONEncode(Settings))
+    Load(FileName,Settings)
 end
 
 getgenv().Update = function(FileName,Settings)
-if not pcall(readfile, FileName) then Set(FileName,Settings) return end
-writefile(FileName, HP:JSONEncode(Settings))
+  local ExistingFile = pcall(readfile, FileName)
+  if not ExistingFile then
+    Set(FileName,Settings)
+  else
+    writefile(FileName, HP:JSONEncode(Settings))
+  end
 end
