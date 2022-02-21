f623976b4654805bb59460782962fe97db5ad43c
MirroxGame <70598667+MirroxGame@users.noreply.github.com>
2022-02-21 15:47:07 +0300

    Update settings.lua

--- settings/settings.lua
+++ settings/settings.lua
@@ -6,25 +6,22 @@ local HP = game:GetService("HttpService")
 
 assert(writefile,"Your exploit does not support writefile function.")
 assert(readfile,"Your exploit does not support readfile function.")
-        
+
 -- //functions
 
 getgenv().Load = function(FileName)
-    return HP:JSONDecode(readfile(FileName))
+return HP:JSONDecode(readfile(FileName))
 end
 
 getgenv().Set = function(FileName,Settings)
-    if FileName:match("/") then
-        makefolder(FileName:sub(1,FileName:find("/") - 1))
-    end
-    writefile(FileName, HP:JSONEncode(Settings))
-    Load(FileName,Settings)
+if FileName:match("/") then
+makefolder(FileName:sub(1,FileName:find("/") - 1))
+end
+writefile(FileName, HP:JSONEncode(Settings))
+Load(FileName,Settings)
 end
 
 getgenv().Update = function(FileName,Settings)
-    local NewSettings = {}
-    for i,v in pairs(Settings) do
-        NewSettings[i] = v
-    end
-    writefile(FileName, HP:JSONEncode(NewSettings))
+if not pcall(readfile, FileName) then Set(FileName,Settings) end
+writefile(FileName, HP:JSONEncode(Settings))
 end
